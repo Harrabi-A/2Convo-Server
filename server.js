749d01e22d7b1aefcf56396ec2710af59cd9b7ef
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-07-03 21:44:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const {Server} = require('socket.io');
 
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const User = require('./models/userSchema')
+const User = require('./models/userSchema');
+const Convo = require('./models/convoSchema')
+const { text } = require('express');
 
 app.use(cors());
 
@@ -21,7 +23,7 @@ app.use(function(req, res, next) {
 var onlineList = []
 var convoRequestList = []
 var inConvoList = []
-var nextCode = 1000        // to generate unique convo Code
+var nextCode = 1000        // minimum convo code
 
 const server = http.createServer();
 
@@ -31,7 +33,7 @@ mongoose.connect(URI)
    .then((result) => {
      console.log("Connected to DB");
 
-     //Start listening only after databasa connection
+     //Start listening only after server got a connection with database
      server.listen(process.env.PORT, () =>{
         console.log("Server Runing",process.env.PORT);
     
@@ -84,39 +86,104 @@ io.on("connection", (socket) => {
         });
     
     socket.on("requestConvo", (convoCode, convoRequested) => {
+      const convoReq = convoCode.toString()+convoRequested.toString()
       if(onlineList.includes(convoRequested)){
         io.to(socket.id).emit("convoIDFound")
         // check a match request 
         console.log("the user ", convoCode, " requested a convo with existing user: ", convoRequested)  
-        if (convoRequestList.includes([convoRequested.toString(), convoCode.toString()])){
+        if (convoRequestList.includes(convoRequested.toString()+convoCode.toString())){
+          var socketID1
+          var socketID2
           // convo request match
-          console.log("convo request match")
-          inConvoList.push([convoRequested, convoCode])
-          convoRequestList.pop([convoRequested.toString(), convoCode.toString()])
-          //
-          // TODO emit to the convoer the start of convo (time stamp, convoID, ..)
-          //
+          inConvoList.push(convoRequested+convoCode)
+          convoRequestList.pop(convoRequested.toString()+convoCode.toString())
+          //retrive socket
+
+          var today= new Date();
+          User.findOne({convoCode: convoCode})
+            .then(obj => {
+              console.log(obj.convoCode," convo code: ",convoCode," has socket: ",obj.socketID)
+              
+              // get the public key of convoer && save convo in database
+              User.findOne({convoCode: convoRequested})
+                 .then(obj1 => {
+                   // save convo in Database
+                   socketID1 = obj.socketID
+                   socketID2 = obj1.socketID
+                   const convo = new Convo({socket1: socketID1, socket2: socketID2})
+                   convo.save()
+                     .catch(err => {console.log(err)})
+                   io.to(obj.socketID).emit("startConvo", convoRequested, today.toString(), obj1.publicKey)
+                 })
+            })
+          
+            User.findOne({convoCode: convoRequested})
+            .then(obj => {
+              console.log(obj.convoCode," convo code: ",convoRequested," has socket: ",obj.socketID)
+              User.findOne({convoCode: convoCode})
+                .then( obj1 => {
+                  io.to(obj.socketID).emit("startConvo", convoCode, today.toString(), obj1.publicKey)
+                } )            
+            })
+            
         } else {
-          // register the request
-          const convoReq = [convoCode, convoRequested]
+          // register the request       
           convoRequestList.push(convoReq)
         }
       } else {
         io.to(socket.id).emit("convoIDNotFound")
       }
+      socket.on("endWaiting", () => {
+        convoRequestList.pop(convoReq)
+      })
       console.log("inConvoList: ", inConvoList)
       console.log("convoRequestList: ", convoRequestList)
     })
     
+    socket.on("sendMessage", (pleinText, pk) => {
+      User.findOne({publicKey: pk})
+        .then(obj => {
+          io.to(obj.socketID).emit("newMessage", pleinText)
+        })
+    })
+
+    
+
+    socket.on("endConvo", () => {
+      console.log("end convo recevied")
+      Convo.findOneAndDelete({socket1: socket.id})
+         .then(obj => {
+           if(obj !== null){
+               io.to(obj.socket2).emit("convoerDisconnected")
+           }
+         })
+      Convo.findOneAndDelete({socket2: socket.id})
+         .then(obj => {
+           if(obj !== null){
+               io.to(obj.socket1).emit("convoerDisconnected")
+           }
+         })
+    })
 
     socket.on("disconnect", () => {
       console.log("User disconnect", socket.id)
       user.remove();
-      //remove from onlineList
-      //
-      //TODO
-      //
-    });
+      onlineList.pop(user.convoCode)
+
+      Convo.findOneAndDelete({socket1: socket.id})
+         .then(obj => {
+           if(obj !== null){
+               io.to(obj.socket2).emit("convoerDisconnected")
+           }
+         })
+      Convo.findOneAndDelete({socket2: socket.id})
+         .then(obj => {
+           if(obj !== null){
+               io.to(obj.socket1).emit("convoerDisconnected")
+           }
+         })
+    })
+      
   });
 
   
